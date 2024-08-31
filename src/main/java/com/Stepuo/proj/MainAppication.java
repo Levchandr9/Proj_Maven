0eff6b8461bf89fc7e0116b7eeb93d3c16bfa0d3
Levchandr9 <levon.vagarshakovich9@gmail.com>
2024-08-31 13:01:23 +0300

    Доработали что-то убрали что-то добавили

--- src/main/java/com/Stepuo/proj/MainAppication.java
+++ src/main/java/com/Stepuo/proj/MainAppication.java
@@ -4,10 +4,8 @@ public class MainAppication {
     public static void main(String[] args) {
         System.out.printf("Hello world");
         System.out.println("Hello World2");
-        System.out.printf("1211");
-        System.out.printf("1211");
-        System.out.printf("1211");
-        System.out.printf("1211");
+
         System.out.printf("23");
-    }
+        System.out.printf("23");
+}
 }
